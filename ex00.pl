f66dad4377bcaf3f3f6988f144f09217962addc6
Spectre <antoine@doussaud.org>
2015-03-28 00:20:53 +0100

    beauxparents

--- ex00.pl
+++ ex00.pl
@@ -5,7 +5,19 @@ femme(sylvie).
 femme(eve).
 femme(julie).
 femme(valerie).
-homme(X) :- not(femme(X)).
+
+
+
+homme(jean).
+homme(jacques).
+homme(herve).
+homme(gerard).
+homme(marc).
+homme(luc).
+homme(jules).
+homme(leon).
+homme(loic).
+homme(paul).
 
 mari_de(marc,anne).
 mari_de(luc,betty).
@@ -13,9 +25,31 @@ mari_de(jules,lisa).
 mari_de(leon,sylvie).
 mari_de(loic,eve).
 mari_de(paul,julie).
+
+homme(X) :- not(femme(X)).
+
 femme_de(X, Y) :- femme(X), mari_de(Y, X).
+
+mari_de(X, Y) :- homme(X), femme_de(Y, X).
+
+enfant_de(X, Y) :- mari_de(Z, Y), enfant_de(X, Z).
 
 enfant_de(jean, marc).
 enfant_de(jules, marc).
-enfant_de(X, Y) :- mari_de(Z, Y), enfant_de(X, Z).
+enfant_de(leon, marc).
+
+enfant_de(lisa, luc).
+enfant_de(loic, luc).
+enfant_de(gerard, luc).
+
+enfant_de(herve, leon).
+enfant_de(julie, leon).
+
+enfant_de(paul, loic).
+enfant_de(valerie, loic).
+
+enfant_de(jacques, jules).
+
+beaupere_de(X, Y) :- (mari_de(Z, Y) ; femme_de(Z, Y)), enfant_de(Z, X), homme(X).
+bellemere_de(X, Y) :- (mari_de(Z, Y) ; femme_de(Z, Y)), enfant_de(Z, X), femme(X).
 
