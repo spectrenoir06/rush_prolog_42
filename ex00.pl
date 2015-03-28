2ec5480ffc620117820088e096003b56c3929955
Vallium <aalliot@student.42.fr>
2015-03-28 12:49:52 +0100

    code propre

--- ex00.pl
+++ ex00.pl
@@ -1,45 +1,17 @@
-femme(anne).
-femme(betty).
-femme(lisa).
-femme(sylvie).
-femme(eve).
-femme(julie).
-femme(valerie).
+femme(X) :- member((X), [anne, betty, lisa, sylvie, eve, julie, valerie]).
+homme(X) :- member((X), [jacques, herve, gerard, marc, luc, jules, leon, loic, paul]).
 
-homme(jacques).
-homme(herve).
-homme(gerard).
-homme(marc).
-homme(luc).
-homme(jules).
-homme(leon).
-homme(loic).
-homme(paul).
-
-mari_de(marc,anne).
-mari_de(luc,betty).
-mari_de(jules,lisa).
-mari_de(leon,sylvie).
-mari_de(loic,eve).
-mari_de(paul,julie).
+mari_de(X, Y) :- member(X, Y), [(marc, anne), (luc, betty), (jules, lisa), (leon, sylvie), (loic, eve), (paul, julie)].
 
 femme_de(X, Y) :- femme(X), mari_de(Y, X).
-
 mari_de(X, Y) :- homme(X), femme_de(Y, X).
 
 enfant_de(X, Y) :- mari_de(Z, Y), enfant_de(X, Z).
 
-enfant_de(jean, marc).
-enfant_de(jules, marc).
-enfant_de(leon, marc).
-enfant_de(lisa, luc).
-enfant_de(loic, luc).
-enfant_de(gerard, luc).
-enfant_de(herve, leon).
-enfant_de(julie, leon).
-enfant_de(paul, loic).
-enfant_de(valerie, loic).
-enfant_de(jacques, jules).
+enfant_de(X, Y) :- member((X, Y), [(leon, marc), (jules, marc), (jean, marc)]). 
+enfant_de(X, Y) :- member((X, Y), [(lisa, luc), (loic, luc), (gerard, luc)]). 
+enfant_de(X, Y) :- member((X, Y), [(herve, leon), (julie, leon)]).
+enfant_de(X, Y) :- member((X, Y), [(paul, loic), (valerie, loic), (jacques, jules)]).  
 
 ancetre_de(X, Y) :- enfant_de(Y, X).
 ancetre_de(X, Z) :- enfant_de(Y, X), ancetre_de(Y, Z).
