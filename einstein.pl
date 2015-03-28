08220a727d31bdab055bbafed98d5d996333f15b
Vallium <aalliot@student.42.fr>
2015-03-28 13:19:27 +0100

    eistein

---
+++ einstein.pl
@@ -0,0 +1,6 @@
+nation(X) :- member((X), [anglais, suedois, danois, norvegien, allemand]).
+maison(X) :- member((X), [rouge, verte, blanche, jaune, bleue]).
+boisson(X) :- member((X), [the, cafe, lait, biere, eau]).
+cigare(X) :- member((X), [pallmall, dunhill, blend, bluemaster, prince]).
+animal(X) :- member((X), [chiens, chats, oiseaux, poisson, cheval]).
+
